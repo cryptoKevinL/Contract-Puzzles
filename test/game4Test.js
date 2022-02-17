24887b0ce734a99f3be943d5d8cd90afd22222b6
cyberKevin <>
2022-02-17 10:31:40 -0700

    group 1, class 2

--- test/game4Test.js
+++ test/game4Test.js
@@ -7,8 +7,11 @@ describe("Game4", function() {
     await game.deployed();
 
     // nested mappings are rough :}
+    const signer0 = ethers.provider.getSigner(0);
+    const address0 = await signer0.getAddress();
+    await game.write(address0);
 
-    await game.win();
+    await game.win(address0);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
--- test/game5Test.js
+++ test/game5Test.js
@@ -3,12 +3,26 @@ const { assert } = require("chai");
 describe("Game5", function() {
   it("should be a winner", async function() {
     const Game = await ethers.getContractFactory("Game5");
-    const game = await Game.deploy();
+    let game = await Game.deploy();
     await game.deployed();
 
     // good luck
+    while (! (await game.isWon())) {
+      let signer = ethers.Wallet.createRandom().connect(ethers.provider);
+      const signerAddress = await signer.getAddress();
 
-    await game.win();
+      //wallet needs ETH to make this TX
+      await ethers.provider.send("hardhat_setBalance", [signerAddress, "0xffffffffffffffffffff"]);
+
+      game = await game.connect(signer);
+      //console.log("address: " + await signer.getAddress());
+      try {
+        await game.win();
+      } catch (error) {
+        //console.log(error);
+        continue;
+      }
+    }
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
